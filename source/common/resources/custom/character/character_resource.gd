bf84ab96701139e335c2128596f8c93fa61a4c27
SlayHorizon <163869998+SlayHorizon@users.noreply.github.com>
2024-09-05 00:03:28 +0200

    Add CharacterResource

---
+++ source/common/resources/custom/character/character_resource.gd
@@ -0,0 +1,31 @@
+class_name CharacterResource
+extends Resource
+
+@export var character_name: String
+
+@export var character_sprite: SpriteFrames
+
+# Stats at level 1
+@export var base_health: int = 30
+@export var base_mana: int = 10
+@export var base_attack_damage: int = 5
+@export var base_magic_power: int = 4
+@export var base_attack_speed: float = 0.7
+@export var base_move_speed: float = 300
+
+# Stats progression per level
+# (Too linear)
+@export var health_per_level: float = 10
+@export var mana_per_level: float = 3
+@export var attack_damage_per_level: float = 2
+@export var magic_power_per_level: float = 1.5
+@export var attack_speed_per_level: float = 0.015
+
+@export var passive_abilities: Array[AbilityResource]
+@export var active_abilities: Array[AbilityResource]
+
+@export var description: String = ""
+
+# Possible character class evolution
+# Example: knight -> [holy knight or dark knight]
+@export var evolution: Array[CharacterResource]
--- source/common/resources/custom/player/player_resource.gd
+++ source/common/resources/custom/player/player_resource.gd
@@ -2,7 +2,11 @@ class_name PlayerResource
 extends Resource
 
 @export var display_name: String = "Player"
-@export var skin_color: String = "green"
+@export var character_class: String = "knight"
+
+@export var golds: int = 0
 @export var inventory: Dictionary = {}
 
 @export var level: int = 0
+
+var current_peer_id: int
