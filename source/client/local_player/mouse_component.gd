bfb18b624aea7c622777485421fce3fe5947c5c5
cadrius <160436277+d-Cadrius@users.noreply.github.com>
2024-09-09 18:21:04 -0400

    mouse_probe for server

--- source/client/local_player/mouse_component.gd
+++ source/client/local_player/mouse_component.gd
@@ -1,3 +1,4 @@
+class_name MouseComponent
 extends Node2D
 
 var mouse_ingame := true
---
+++ source/server/server_probe/camera_probe.gd
@@ -0,0 +1,67 @@
+class_name CameraProbe
+extends Camera2D
+
+var mouse: MouseComponent
+
+var desired_position: Vector2 = Vector2.ZERO
+
+var direction: float = 0
+
+var scrolling: Dictionary = {
+	"desired": 1.0,
+	"desired_speed": 25.0,
+	"live": 1.0,
+	"min": 0.5,
+	"max": 10.0,
+	"speed": 0.25
+}
+
+var speed_mul: Dictionary = {
+	"desired": 1.0,
+	"desired_speed": 25.0,
+	"live": 1.0,
+	"min": 1.0,
+	"max": 5.0,
+	"speed": 0.25
+}
+
+func _init() -> void:
+	mouse = MouseComponent.new()
+	add_child(mouse)
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("action"):
+		direction = atan2(mouse.position.y - global_position.y, mouse.position.x - global_position.x)
+		
+		global_position += Vector2.RIGHT.rotated(direction) * (global_position.distance_to(mouse.position) / 10) * speed_mul["live"]
+	
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var ctrl: bool = Input.is_key_pressed(KEY_CTRL)
+		
+		match event.button_index:
+			MOUSE_BUTTON_WHEEL_UP:
+				if ctrl:
+					speed_mul["desired"] = clampf(speed_mul["desired"] + speed_mul["speed"], speed_mul["min"], speed_mul["max"])
+					return
+					
+				scrolling["desired"] = clampf(scrolling["desired"] + scrolling["speed"], scrolling["min"], scrolling["max"])
+			MOUSE_BUTTON_WHEEL_DOWN:
+				if ctrl:
+					speed_mul["desired"] = clampf(speed_mul["desired"] - speed_mul["speed"], speed_mul["min"], speed_mul["max"])
+					return
+				
+				scrolling["desired"] = clampf(scrolling["desired"] - scrolling["speed"], scrolling["min"], scrolling["max"])
+	
+
+func _process(_delta: float) -> void:
+	queue_redraw()
+
+	scrolling["live"] = move_toward(scrolling["live"], scrolling["desired"], _delta * scrolling["desired_speed"])
+	speed_mul["live"] = move_toward(speed_mul["live"], speed_mul["desired"], _delta * speed_mul["desired_speed"])
+	zoom = Vector2(scrolling["live"], scrolling["live"])
+	
+func _draw() -> void:
+	if Input.is_action_pressed("action"):
+		draw_line(Vector2.ZERO, mouse.position - global_position, Color.WHITE, 1.0)
