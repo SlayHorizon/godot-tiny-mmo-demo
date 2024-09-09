40c8939d5031a322fd1d99ac1b41edae9f0d65cf
PantheonIsAtreus <tartelette-raffinee.0f@icloud.com>
2024-09-09 21:35:45 +0200

    Add HUD

--- source/client/ui/ui.gd
+++ source/client/ui/ui.gd
@@ -1,9 +1,9 @@
 extends CanvasLayer
 
-const CHAT_SCENE = preload("res://source/client/ui/chat/chat.tscn")
+const HUD = preload("res://source/client/ui/hud/hud.tscn")
 
 func _ready() -> void:
 	var login_menu: LoginMenu = $LoginMenu
 	await login_menu.connection_succeed
 	login_menu.queue_free()
-	add_child(CHAT_SCENE.instantiate())
+	add_child(HUD.instantiate())
